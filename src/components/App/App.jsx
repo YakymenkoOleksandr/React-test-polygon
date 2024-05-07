d4b52de3a64f20d454935b02091e83486e1708ca
YakymenkoOleksandr <93155369+YakymenkoOleksandr@users.noreply.github.com>
2024-05-07 18:50:22 +0300

    addcomit1

---
+++ src/components/App/App.jsx
@@ -0,0 +1,9 @@
+import css from './App.module.css'
+import Form from '../Form/Form.jsx'
+export default function App() {
+    return (
+        <div className={css.wrapperForAllModules}>
+            <Form />
+        </div>
+    );
+}
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,10 @@
+import css from './Form.module.css'
+export default function Form() {
+    
+    return (
+        <div className={css.formTest}>
+            
+        </div>
+    );
+
+}
